7238f9ed15d9dad33fc64940caa9c997a7e349ec
Fatima35202 <162595090+Fatima35202@users.noreply.github.com>
2025-03-05 01:39:09 -0700

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const notesSlice = createSlice({
+  name: 'notes',
+  initialState: [],
+  reducers: {
+    addNote: (state, action) => { state.push(action.payload); },
+    removeNote: (state, action) => state.filter(note => note.id !== action.payload),
+  },
+});
+
+export const { addNote, removeNote } = notesSlice.actions;
+export default notesSlice.reducer;
